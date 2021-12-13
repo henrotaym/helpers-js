403ba6dd73953c95fc3c2f3312826f8b288cecb6
Mathieu Henrotay <mathieu.henrotay@gmail.com>
2021-12-13 12:37:55 +0100

    🏷 Wrong typing for trying.

--- src/trying.ts
+++ src/trying.ts
@@ -1,5 +1,7 @@
 /** Trying to execute given callback. */
-async function trying<R>(callback: () => R): Promise<[null | Error, null | R]> {
+async function trying<R>(
+  callback: () => R
+): Promise<[null | Error, null | Awaited<R>]> {
   try {
     const response = await callback();
     return [null, response];
